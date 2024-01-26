ed9b1aa54567c8b22ae42c4c21668caefc3db9a3
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-01-26 10:15:00 +0530

    Initial Commit

---
+++ Dose Deck/Dose_DeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dose_DeckApp.swift
+//  Dose Deck
+//
+//  Created by Taranjeet Singh Bedi on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Dose_DeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
