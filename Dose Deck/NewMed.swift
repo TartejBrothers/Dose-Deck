835e8f968660983569aa52158f972dd8dc70227e
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-01-27 01:59:04 +0530

    Updated: New Data Type added

--- Dose Deck/NewMed.swift
+++ Dose Deck/NewMed.swift
@@ -8,51 +8,75 @@
 import SwiftUI
 
 struct NewMed: View {
-    @ObservedObject var datamanager = DataManager()
+    @EnvironmentObject var datamanager: DataManager
+    @State private var newmed = ""
+    @State private var newhours: Int?
+    @State private var newminutes: Int?
+    @State private var isSelected: Bool = false
+    
 
     var body: some View {
-        VStack {
-            if datamanager.med.isEmpty {
-                Text("No medicines available")
-            } else {
-                List {
-                    ForEach(datamanager.med) { meds in
-                        HStack {
-                            Toggle(isOn: $datamanager.med[getIndex(for: meds)].isSelected) {
-                                VStack(alignment: .leading) {
-                                    Text("Medicine: \(meds.medicine)")
-                                    Text("Hours: \(meds.hours)")
-                                    Text("Minutes: \(meds.minutes)")
-                                }
-                            }
-                        }
-                    }
-                }
-              
-            }
+           VStack(alignment: .center, spacing: 20) {
+               Text("Add Details")
+                   .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                   .foregroundColor(.white)
+                   .padding(.bottom,10)
+               Text("Medicine")
+                   .offset(x:-130)
+                   .foregroundColor(.white)
+                   .padding(.bottom,-10)
+               
+               TextField("", text: $newmed)
+                   .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+                   .foregroundColor(.white)
+                   .background(
+                       RoundedRectangle(cornerRadius: 25)
+                           .stroke(Color.gray, lineWidth: 1.0)
+                   )
+               Text("Hour Clock")
+                   .offset(x:-125)
+                   .foregroundColor(.white)
+                   .padding(.bottom,-10)
+               
+               TextField("", value: $newhours, formatter: NumberFormatter())
+                   .keyboardType(.numberPad)
+                   .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+                   .foregroundColor(.white)
+                   .background(
+                       RoundedRectangle(cornerRadius: 25)
+                           .stroke(Color.gray, lineWidth: 1.0)
+                   )
+               Text("Minute Clock")
+                   .offset(x:-117)
+                   .foregroundColor(.white)
+                   .padding(.bottom,-10)
+               TextField("", value: $newminutes, formatter: NumberFormatter())
+                   .keyboardType(.numberPad)
+                   .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+                   .foregroundColor(.black)
+                   .background(
+                       RoundedRectangle(cornerRadius: 25)
+                           .stroke(Color.gray, lineWidth: 1.0)
+                   )
+               Toggle("Notfication", isOn: $isSelected)
+                               .padding()
+                               .foregroundColor(.white)
 
-            Button{
-                
-            } label: {
-                Text("Logout")
-                    .padding()
-                    .background(Color.red)
+               Button {
+                   // Handle button action
+               } label: {
+                   Text("Save")
+                       .frame(width: 100, height: 40)
+                       .background(RoundedRectangle(cornerRadius: 20, style: .continuous).fill(Color(.black)))
                     .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-        }
-        
-    }
-
-    // Helper function to get the index of a DataType in the array
-    private func getIndex(for medicine: DataType) -> Int {
-        if let index = datamanager.med.firstIndex(where: { $0.id == medicine.id }) {
-            return index
-        }
-        return 0
-    }
+               }
+           }
+           .frame(maxHeight: .infinity)
+           .padding(.horizontal, 20)
+           .background(Color(red: 34.0/255.0, green: 40.0/255.0, blue: 49.0/255.0, opacity: 1.0))
+           .ignoresSafeArea()
+       }
 }
-
 
 #Preview {
     NewMed()
